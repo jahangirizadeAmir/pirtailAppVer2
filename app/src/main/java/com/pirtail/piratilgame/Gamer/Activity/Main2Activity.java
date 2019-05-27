3a3c9344ba2178a9f463624c0561c411ecc7eace
SARNM <dalireza1374@gmail.com>
2019-05-27 17:41:17 +0430

    define some views in java Main2Activity...

--- app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/Main2Activity.java
+++ app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/Main2Activity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.pirtail.piratilgame.R;
 
 public class Main2Activity extends AppCompatActivity {
 
     EditText edt_username, edt_phone_number, edt_name_family;
     RadioButton rbtn_men, rbtn_women;
+    String username, phoneNumber, nameFamily, gender;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +28,20 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void defineObjects() {
-        edt_username=(EditText)findViewById(R.id.edt_phone_number);
+        edt_username=(EditText)findViewById(R.id.edt_username);
         edt_phone_number=(EditText)findViewById(R.id.edt_phone_number);
         edt_name_family=(EditText)findViewById(R.id.edt_name_family);
         rbtn_men=(RadioButton) findViewById(R.id.rbtn_men);
         rbtn_women=(RadioButton) findViewById(R.id.rbtn_women);
+
+        username = edt_username.getText().toString().trim();
+        phoneNumber = edt_phone_number.getText().toString().trim();
+        nameFamily = edt_name_family.getText().toString().trim();
+        if (rbtn_men.isChecked()){
+            gender="1";
+        }else {
+            gender="0";
+        }
+
     }
 }
