4cb69a5cf85f5e4410ad118ffc99999435501ef1
SARNM <dalireza1374@gmail.com>
2019-05-27 17:01:20 +0430

    define edt_phone_number and btn_send

--- app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/MainActivity.java
+++ app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/MainActivity.java
@@ -2,14 +2,30 @@ package com.pirtail.piratilgame.Gamer.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pirtail.piratilgame.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText edt_phone_number;
+    Button btn_send;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        defineObjects();
+
+
+
+    }
+
+    private void defineObjects() {
+        edt_phone_number=(EditText)findViewById(R.id.edt_phone_number);
+        btn_send=(Button)findViewById(R.id.btn_send);
     }
 }
