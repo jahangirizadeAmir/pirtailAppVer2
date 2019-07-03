5426af0a118ad4796413d3d628f335ee7326dd20
SARNM <dalireza1374@gmail.com>
2019-07-03 17:38:36 +0430

    complete several activities

--- app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/MapActivity.java
+++ app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/MapActivity.java
@@ -1,22 +1,42 @@
 package com.pirtail.piratilgame.Gamer.Activity;
 
 import android.content.Context;
-import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.pirtail.piratilgame.R;
 
+import java.util.Objects;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MapActivity extends AppCompatActivity {
 
     @BindView(R.id.mapView)
-            MapView mapView;
+    MapView mapView;
+    @BindView(R.id.search_bar)
+    EditText searchBar;
+    AlertDialog.Builder alertDialog;
+    LayoutInflater layoutInflater;
+    View view;
+    AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,31 @@ public class MapActivity extends AppCompatActivity {
 
         mapView.onCreate(savedInstanceState);
         mapView.setStyleUrl(Style.MAPBOX_STREETS);
+
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                alertDialog=new AlertDialog.Builder(MapActivity.this);
+                layoutInflater= getLayoutInflater();
+                view=layoutInflater.inflate(R.layout.search_layout, null);
+                alertDialog.setView(view);
+                dialog=alertDialog.create();
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
 
     }
@@ -71,4 +116,13 @@ public class MapActivity extends AppCompatActivity {
         mapView.onLowMemory();
     }
 
+//    @OnClick(R.id.search_bar)
+//    public void onViewClicked() {
+//        alertDialog=new AlertDialog.Builder(MapActivity.this);
+//        layoutInflater= getLayoutInflater();
+//        view=layoutInflater.inflate(R.layout.search_layout, null);
+//        alertDialog.setView(view);
+//        dialog=alertDialog.create();
+//        dialog.show();
+//    }
 }
