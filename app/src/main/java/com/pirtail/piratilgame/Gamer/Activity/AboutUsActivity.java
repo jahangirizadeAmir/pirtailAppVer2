bec29bd9030413764f2f95c0dbc503cfbaa37216
SARNM <dalireza1374@gmail.com>
2019-07-04 11:46:50 +0430

    new change 13-5-98

--- app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/AboutUsActivity.java
+++ app/src/main/java/com/pirtail/piratilgame/Gamer/Activity/AboutUsActivity.java
@@ -1,16 +1,31 @@
 package com.pirtail.piratilgame.Gamer.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.pirtail.piratilgame.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AboutUsActivity extends AppCompatActivity {
+
+    @BindView(R.id.ic_back)
+    ImageView icBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.ic_back)
+    public void onViewClicked() {
+        onBackPressed();
     }
 }
